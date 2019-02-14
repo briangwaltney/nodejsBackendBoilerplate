9fa0f70d18d234c6e3a8f76d2c108dbe044d2f60
Brian Gwaltney <brian.gwaltney@gmail.com>
2019-02-14 16:28:44 +0700

    cleaned config test

--- tests/unit/startup/config.test.js
+++ tests/unit/startup/config.test.js
@@ -4,7 +4,6 @@ const config = require('config');
 describe('config file', () => {
   it('should throw error if jwtPrivateKey not defined', () => {
     config.get = function (jwtPrivateKey) {
-      console.log('Fake config.get() function')
       return jwtPrivateKey = '';
     };
     expect(() => { configStartup() }).toThrow();
