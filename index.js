fc18e4b1f642506aa90f3fd3320190bc74b07a16
Brian Gwaltney <brian.gwaltney@gmail.com>
2019-02-21 15:51:34 +0700

    added .env setup

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-//FEAR SETTING
+//Boiler Plate
 
 //Dependencies and Setup
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const winston = require('winston');
 require('winston-mongodb');
+
 
 //Modules I've created
 require('./startup/logging')(); //uses winston to start logging
@@ -12,6 +14,8 @@ require('./startup/db')(); //connects to mongodb
 require('./startup/routes')(app); //sets up route handling
 require('./startup/config')(); //ensures private key is set
 require('./startup/validation')(); //sets up Joi and allows access to ObjectId()
+
+
 
 //3001 selected because it will be used as proxy in React app
 const port = process.env.PORT || 3001;
